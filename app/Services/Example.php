8cf8204929f9e95fe60b91e059a3b7f5254692d9
Carlos Ferreira <carlos@especializati.com.br>
2022-11-02 15:37:31 -0300

    feat: managing memory leaks

---
+++ app/Services/Example.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Services;
+
+class Example
+{
+    public static $values;
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,13 @@
 <?php
 
+use App\Services\Example;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/test', function () {
+    Example::$values[] = rand(4, 2);
+
+    dd(Example::$values);
+});
 
 Route::get('/', function () {
     return view('welcome');
